aee30dedc071121e5ce09070990c2a2e1e0834a7
Celso <celsoborealis@gmail.com>
2013-11-03 21:44:03 -0800

    Now, I have a dirty version of a waveform to obspy converter
    To get waveforms from a .mat file....
    >>> w = loadmatwaveforms("somewave.mat")
    
    So, assume a file was saved from matlab...
    > mywaveforms = [waveform(), waveform(), waveform()];
    > differentwave = [waveform];
    > some_other_variable = 3;
    > save mystuff
    
    Then
    >>> w = loadmatwaveforms("mystuff.mat")
    will return w as a dictionary...
    w['mywaveforms']= <osbpy.core.stream.Stream>  # containing 3 individual
    Traces
    w['otherwave'] = <obspy.core.stream.Stream> #containing 1 Trace

--- waveformconversion.py
+++ waveformconversion.py
@@ -6,30 +6,53 @@ Helper to retrieve matlab waveform data into python.
 from datetime import (datetime, timedelta)
 import numpy as np
 import scipy.io as sio
+from obspy.core.trace import Trace
+import obspy.core.stream as obsstream
 
 class Waveform:
-    def __init__(self):
+    def __init__(self, matlabfile=None, matlabvariable=None):
+        if matlabfile:
+             self.create_from_mat(vnames=matlabvariable,
+                        matfilename=matlabfile)
+             return
         self.network = None
         self.station = None
         self.location = None
         self.channel = None
-        self.sample_rate = 0
-        self.start_time = datetime.now()
+        self.sampling_rate = 0.0
+        self.starttime = datetime.now()
         self.data = np.empty(0)
         self.units = 'unknown'
         self.version = 0
         self.misc_fields = {}
         self.history = []
         
-    def waveform2python(self,w, yn_to_tf=True):
+    def python2obspy(self):
+        from obspy.core.trace import Stats, Trace
+        from obspy.core.utcdatetime import UTCDateTime
+        s = Stats()
+        s.network = self.network
+        s.station = self.station
+        s.location = self.location
+        s.channel = self.channel
+        s.sampling_rate = self.sampling_rate
+        s.starttime = UTCDateTime(self.starttime)
+        s.npts = len(self.data)
+        misc_fields=dict()
+        if self.misc_fields.has_key('CALIB'):
+            s.calib = self.misc_fields.pop('CALIB')
+        s.update(self.misc_fields)
+        return Trace(self.data[:], header=s)    
+        
+    def waveform2python(self, w, yn_to_tf=True):
             self.network = str(w.scnl.network)
             self.station = str(w.scnl.station)
             self.location = str(w.scnl.location)
             self.channel = str(w.scnl.channel)
-            self.sample_rate = w.Fs
+            self.sampling_rate = w.Fs
             ordinal_time = int(w.start)
             delta_time = ordinal_time - w.start
-            self.start_time = datetime.fromordinal(ordinal_time) - timedelta(ordinal_time - w.start)
+            self.starttime = datetime.fromordinal(ordinal_time) - timedelta(ordinal_time - w.start)
             self.data = w.data
             self.units = w.units
             self.version = 0
@@ -42,12 +65,13 @@ class Waveform:
                     except AttributeError:
                         pass
             self.history = []
+            return self # for chaining
             
     def __repr__(self):
         myrepr=[]
         myrepr.append("net: [%s], sta:[%s], loc:[%s], cha:[%s]" % (
             self.network, self.station, self.location, self.channel))
-        myrepr.append("starting: [%s]" % self.start_time)
+        myrepr.append("starting: [%s]" % self.starttime)
         myrepr.append("data: %d samples   ranging from [%f to %f]" % (
             len(self.data), min(self.data),max(self.data)))
         myrepr.append("units: %s" % self.units)
@@ -58,7 +82,7 @@ class Waveform:
         return '\n'.join(myrepr)
                        
 
-        
+    '''    
     def create_from_mat(self,vnames=['rms_waveforms'],
                         matfilename="C:\Users\Celso\Desktop\RMS20030926.mat"):
         """ Return a dictionary of waveform lists. Each entry is a variable"""
@@ -76,4 +100,41 @@ class Waveform:
         # print waveforms['rms_waveforms']
         
         self.waveform2python(waveforms['rms_waveforms'][0])
+    '''
+    
+def loadmatwaveforms(matfilename=None, vnames=None):
+    """ load from a file into a dictionary of obspy streams
+    
+    specify specific variable with vnames
+    results are returned with keys as variable names
+    """
+    def is_waveform(an_item):
+        try:
+            return an_item[1].classname == 'waveform'
+        except AttributeError:
+            return False
+        
+    m = sio.loadmat(matfilename,mat_dtype=True,squeeze_me=True,
+                        struct_as_record=False, variable_names=vnames)
+    loaded_waveforms = dict([x for x in m.items() if is_waveform(x)])
+    result={}
+    for vname in  loaded_waveforms:
+        #for one_waveform in loaded_waveforms[vname]:
+           # print one_waveform
+        w = Waveform()
+        result[vname] = obsstream.Stream([w.waveform2python(one_waveform).python2obspy() for one_waveform in loaded_waveforms[vname] ])
+        # print result[vname]
+    return result
+    
+w = loadmatwaveforms("C:\Users\Celso\Desktop\RMS20030926.mat")
+obsw={}
+print w
+for k in w:
+    print k
+    w[k].plot()
+    for t in w[k]:
+        print(t)
+    
+    
+    
     
